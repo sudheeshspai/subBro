37e8750e491664d6d1dd3de29955ff57aa19d2fb
Mr:Pai <115912414+sudheeshspai@users.noreply.github.com>
2025-01-24 22:51:20 +0530

    Update subBro.sh

--- subBro.sh
+++ subBro.sh
@@ -1,10 +1,5 @@
-#!/bin/bash
-#1. user wii enter the domain name
-#2. the script will fetch the subdomains of the domain in jason format
-#3. the script will parse the jason file and the subdomains(subdomain.txt) in a file
-#defining the and output file
+#!/bin/bash\                    
 read -p "SubBro here (^_^)Enter the domain name: " domain
 output="subdomain.txt"
-#fetching the subdomains
 curl -s "https://crt.sh/?q=%.$domain" | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u > $output
 echo "The subdomains are saved in $output (^_^) happy hacking"
