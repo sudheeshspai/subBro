9f702cd6c6dfcbdea55ff11f231a14fcf0a34bb1
Mr:Pai <115912414+sudheeshspai@users.noreply.github.com>
2025-01-25 13:01:00 +0530

    Update subBro.sh
    added subro word in shell script to make it more attractive

--- subBro.sh
+++ subBro.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo " __           __  __  __"
+echo "|__  |  ||__ |__||__||  |"
+echo " __| |__||__||__||  \|__|"
+echo ""
 read -p "SubBro here (^_^)Enter the domain name: " domain
 read -p "enter the file name to save the subdomains of $domain :" output                    
 curl -s "https://crt.sh/json?q=$domain" | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u > $output
