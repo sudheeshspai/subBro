ec017edac047a9029fb67a4d988409fb0a03e514
Mr:Pai <115912414+sudheeshspai@users.noreply.github.com>
2025-01-25 12:37:57 +0530

    Update subBro.sh
    there was a small error (fixed) happy hacking

--- subBro.sh
+++ subBro.sh
@@ -1,5 +1,5 @@
-#!/bin/bash\                    
+#!/bin/bash
 read -p "SubBro here (^_^)Enter the domain name: " domain
-output="subdomain.txt"
-curl -s "https://crt.sh/?q=%.$domain" | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u > $output
+read -p "enter the file name to save the subdomains of $domain :" output                    
+curl -s "https://crt.sh/json?q=$domain" | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u > $output
 echo "The subdomains are saved in $output (^_^) happy hacking"
